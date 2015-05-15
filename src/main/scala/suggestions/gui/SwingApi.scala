1e633d92f4ae2eba2623cd7c747cc95ccf2a0153
lost_mohican <erkinun@gmail.com>
2015-05-16 00:43:29 +0300

    used subscription

--- src/main/scala/suggestions/gui/SwingApi.scala
+++ src/main/scala/suggestions/gui/SwingApi.scala
@@ -9,7 +9,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{ Try, Success, Failure }
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
-import rx.lang.scala.Observable
+import rx.lang.scala.{Subscription, Observable}
 
 /** Basic facilities for dealing with Swing-like components.
 *
@@ -51,7 +51,15 @@ trait SwingApi {
       * @param field the text field
       * @return an observable with a stream of text field updates
       */
-    def textValues: Observable[String] = Observable.from(List(field.text))
+    def textValues: Observable[String] = Observable.create { observer =>
+      val subscription = Subscription()
+
+      while (subscription.isUnsubscribed) {
+        observer.onNext(field.text)
+      }
+      observer.onCompleted()
+      subscription
+    }
 
   }
 
@@ -62,7 +70,14 @@ trait SwingApi {
      * @param field the button
      * @return an observable with a stream of buttons that have been clicked
      */
-    def clicks: Observable[Button] = Observable.from(List(button))
+    def clicks: Observable[Button] = Observable.create { observer =>
+      val subscription = Subscription()
+      while (subscription.isUnsubscribed) {
+        observer.onNext(button)
+      }
+      observer.onCompleted()
+      subscription
+    }
   }
 
 }
