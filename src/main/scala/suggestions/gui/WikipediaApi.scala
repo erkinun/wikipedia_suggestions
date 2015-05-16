2604b22210376dd30e192ca16f22412c6ae98bcb
lost_mohican <erkinun@gmail.com>
2015-05-16 15:51:52 +0300

    impl recovered

--- src/main/scala/suggestions/gui/WikipediaApi.scala
+++ src/main/scala/suggestions/gui/WikipediaApi.scala
@@ -48,7 +48,7 @@ trait WikipediaApi {
      *
      * E.g. `1, 2, 3, !Exception!` should become `Success(1), Success(2), Success(3), Failure(Exception), !TerminateStream!`
      */
-    def recovered: Observable[Try[T]] = ???
+    def recovered: Observable[Try[T]] = obs map { vl => Try(vl) } onErrorReturn { throwable => Failure(throwable) }
 
     /** Emits the events from the `obs` observable, until `totalSec` seconds have elapsed.
      *
